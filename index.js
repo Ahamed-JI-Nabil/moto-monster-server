6e147b3496c72526aaca47221739f439dc8911ab
NABIL <johayeriqbal@gmail.com>
2022-05-27 19:42:42 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
@@ -7,6 +8,31 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.expmf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+
+    try {
+        await client.connect()
+
+
+    }
+    finally {
+
+
+    }
+}
+
+run().catch(console.dir)
+
+
+
+
 
 
 
