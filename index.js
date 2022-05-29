d6393140c96be0bf2c62deae3fb855b1d9479eb8
NABIL <johayeriqbal@gmail.com>
2022-05-29 14:15:17 +0600

    added find order by user email and product update quantity api

--- index.js
+++ index.js
@@ -43,6 +43,31 @@ async function run() {
         })
 
 
+        app.get('/orders/:email', async (req, res) => {
+            const email = req.params.email
+            const result = await ordersCollection.find({ userEmail: email }).toArray()
+            res.send(result)
+        })
+
+
+
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const updateItemQuantity = req.body
+            const query = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    availableQuantity: updateItemQuantity.availableQuantity
+                }
+            }
+            const result = await productsCollection.updateOne(query, updatedDoc, options)
+            res.send(result)
+        })
+
+
+
+
     }
     finally {
 
